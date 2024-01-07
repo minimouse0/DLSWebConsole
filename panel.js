16043ab011df37aa890615992b2792899a9bb429
minimouse0 <473147334@qq.com>
2024-01-07 23:37:32 +0800

    power, restart and backup button

--- panel.js
+++ panel.js
@@ -40,3 +40,57 @@ function execute(cmd,callback){
 function executeCustomCmd(form,event){
 	execute(form.value,()=>{});
 }
+function get_server_status(callback){
+	var settings = {
+		"url": host+"/process_status",
+		"method": "GET",
+		"timeout": 0,
+	};
+	$.ajax(settings).done((response)=>{
+		callback(response.online);
+	});
+}
+
+function _switch(){
+	//首先获取服务器是否开启，然后执行开机或关机
+		switch_button=document.getElementById('power');
+		get_server_status((status)=>{
+			if(status){//服务器开启则执行关机
+				switch_button_color(switch_button,1);
+				execute("stop");
+			}
+			else{//服务器关闭则执行开机
+				switch_button_color(switch_button,0);
+				execute("start");
+			}
+			switch_button_wait_status_change(switch_button,status);//执行开关命令后等待改变开关按钮颜色
+		})
+
+
+}
+function switch_button_wait_status_change(switch_button,old_status){
+		let switch_button_task_id=setInterval(()=>{//轮询进程状态等待其改变
+			get_server_status((current_status)=>{
+				if(current_status!=old_status){//一旦有一次请求发现状态改变
+					console.log(current_status)
+					switch_button_color(switch_button,Number(current_status));
+					clearInterval(switch_button_task_id);//既然已经发现状态改变，执行完改变后立刻停止轮询
+				}
+			})
+		},500)		
+
+}
+function switch_button_color(switch_button,status){
+	switch(status){
+		case 0:switch_button.style["background-color"]="#bbb";break;
+		case 1:switch_button.style["background-color"]="green";break;
+	}
+	
+}
+function executeBak(){
+    execute('startBak');
+    setTimeout(()=>{get_server_status()},200);
+}
+function executeRestart(){
+    execute("restart");
+}
