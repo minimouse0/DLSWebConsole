e800ad17f2db43ddaa00d10a044aa9e22ba86f60
dlcn <microccl@foxmail.com>
2024-03-17 17:19:06 +0800

    重构与修复cookie顺序导致的bug

--- js/panel.js
+++ js/panel.js
@@ -1,14 +1,26 @@
-const host=document.cookie.split(";")[0].match(/host=(.+)/)[1];
-const token=document.cookie.split(";")[1].match(/token=(.+)/)[1];
+function parseCookie() {
+	cookie_data = {}
+	let ca = document.cookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let item = ca[i].trim().split("=");
+		if (item.length > 1) {
+			cookie_data[item[0]] = item[1];
+		}
+	}
+	return cookie_data;
+}
+cookie_data = parseCookie();
+const host = cookie_data["host"];
+const token = cookie_data["token"];
 
 /**
  * 向控制台发送指令
  * @param {string} cmd 指令
  * @param callback 收到发送结果后执行的回调函数
  */
-function execute(cmd,callback){
+function execute(cmd, callback) {
 	var settings = {
-		"url": host+"/execute",
+		"url": host + "/execute",
 		"method": "POST",
 		"timeout": 0,
 		"headers": {
@@ -26,17 +38,17 @@ function execute(cmd,callback){
 		//这里做成如果发送成功就调用css让执行键闪一下绿灯
 		//console.log(response);
 
-        /*
+		/*
 		if(response.msg=="提交命令成功!"){
 			let execute_button=document.getElementById('execute');
 			execute_button.style["background-color"]="#77fc5ccf"
 			setTimeout(()=>execute_button.style["background-color"]="#bbb",150);
 		}
-        */
+		*/
 	});
 }
 
 //由用户输入指令并点击执行按钮
-function executeCustomCmd(form,event){
-	execute(form.value,()=>{});
+function executeCustomCmd(form, event) {
+	execute(form.value, () => { });
 }
